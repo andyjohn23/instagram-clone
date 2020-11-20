c5099f8bec48c481170142ecd613502fd4d8a46a
andyjohn23 <andrewnick906@gmail.com>
2020-11-20 14:12:37 +0300

    added the registration form

--- instagram/instausers/views.py
+++ instagram/instausers/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from .forms import RegisterUserForm
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'instausers/index.html')
+    form = RegisterUserForm()
+
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context = {'form':form}
+    return render(request, 'instausers/index.html', context)
+
+def register(request):
+    form = RegisterUserForm()
+
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+    context = {'form':form}
+    return render(request, 'instausers/register.html', context)
+
+
+
  
 
