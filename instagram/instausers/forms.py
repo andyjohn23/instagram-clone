2ef3614e13dcc322afdc5e9648f51c2dfb001e4b
andyjohn23 <andrewnick906@gmail.com>
2020-11-21 14:12:49 +0300

    updated the registration forms added validation messages

--- instagram/instausers/forms.py
+++ instagram/instausers/forms.py
@@ -1,26 +1,27 @@
 from django import forms
-from instausers.models import UserAccount
+from .models import UserAccount
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField(max_length=255, help_text='Required valid emailaddress')
+    email = forms.EmailField(max_length=200, help_text='Required valid emailaddress')
 
     class Meta:
         model = UserAccount
         fields = ['email', 'username', 'password1', 'password2']
 
-def clean_email(self):
-    email = self.cleaned_data['email'].lower()
-    try:
-        account = UserAccount.objects.get(email=email)
-    except Exception as e:
-        return email
-    raise forms.ValidationError(f'Email {email} is already in use!')
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        try:
+            account = UserAccount.objects.get(email=email)
+        except Exception as e:
+            return email
+        raise forms.ValidationError(f'Email {email} is already in use!')
 
-def clean_username(self):
-    username = self.cleaned_data['username']
-    try:
-        account = UserAccount.objects.get(username=username)
-    except Exception as e:
-        return username
-    raise forms.ValidationError(f'Username {username} is already in use!')
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            account = UserAccount.objects.get(username=username)
+        except Exception as e:
+            return username
+        raise forms.ValidationError(f'Username {username} is already in use!')
--- instagram/instausers/views.py
+++ instagram/instausers/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
 from .forms import RegisterUserForm
 
 # Create your views here.
@@ -6,16 +8,28 @@ from .forms import RegisterUserForm
 def index(request):
     return render(request, 'instausers/index.html')
 
-def register(request):
-    if request.method == 'POST':
+def register(request, *arg, **kwargs):
+    user = request.user
+    if user.is_authenticated:
+        return HttpResponse('You are already authenticated as {user.email}.')
+    context = {}
+
+    if request.POST:
         form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            email.cleaned_data.get('email').lower()
+            raw_password = form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            login(request, account)
+            destination = kwargs.get('next')
+            if destination:
+                return redirect(destination)
+            return redirect('register')
+        else:
+            context['register_form'] = form
 
-    else:
-        form = RegisterUserForm()
-    return render(request, 'instausers/register.html', {'form': form, 'title': 'register'})
+    return render(request, 'instausers/register.html', context)
 
 
 
