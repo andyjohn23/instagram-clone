5e63d19e42dfcf5b40f451db74d4cc56c162ef0f
andyjohn23 <andrewnick906@gmail.com>
2020-11-22 02:28:40 +0300

    updated the user-detail template

--- instagram/instausers/urls.py
+++ instagram/instausers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('user-details/', views.user_details, name='user-details')
 ]
