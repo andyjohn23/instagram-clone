180ffd35dc31611d2e6d82c94f6c33f5d158aeb1
andyjohn23 <andrewnick906@gmail.com>
2020-11-21 14:29:26 +0300

    updated the register view

--- instagram/instausers/views.py
+++ instagram/instausers/views.py
@@ -10,22 +10,23 @@ def index(request):
 
 def register(request, *arg, **kwargs):
     user = request.user
+
     if user.is_authenticated:
-        return HttpResponse('You are already authenticated as {user.email}.')
+        return HttpResponse('You are already authenticated as { user.email }.')
     context = {}
 
     if request.POST:
         form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
-            email.cleaned_data.get('email').lower()
+            email = form.cleaned_data.get('email').lower()
             raw_password = form.cleaned_data.get('password1')
             account = authenticate(email=email, password=raw_password)
             login(request, account)
             destination = kwargs.get('next')
             if destination:
                 return redirect(destination)
-            return redirect('register')
+            return redirect('index')
         else:
             context['register_form'] = form
 
